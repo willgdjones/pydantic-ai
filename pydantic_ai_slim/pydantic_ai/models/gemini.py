1cf76e6d1b560b84111784b3027d07a6d7c5dc36
Sydney Runkle <54324534+sydney-runkle@users.noreply.github.com>
2025-01-30 09:18:36 -0500

    Adding new gemini experimental models (#811)

--- pydantic_ai_slim/pydantic_ai/models/gemini.py
+++ pydantic_ai_slim/pydantic_ai/models/gemini.py
@@ -40,7 +40,13 @@ from . import (
 )
 
 GeminiModelName = Literal[
-    'gemini-1.5-flash', 'gemini-1.5-flash-8b', 'gemini-1.5-pro', 'gemini-1.0-pro', 'gemini-2.0-flash-exp'
+    'gemini-1.5-flash',
+    'gemini-1.5-flash-8b',
+    'gemini-1.5-pro',
+    'gemini-1.0-pro',
+    'gemini-2.0-flash-exp',
+    'gemini-2.0-flash-thinking-exp-01-21',
+    'gemini-exp-1206',
 ]
 """Named Gemini models.
 
