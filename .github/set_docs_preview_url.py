f85048d243128c32e0794423d6b871c787650f3c
Samuel Colvin <s@muelcolvin.com>
2025-02-19 17:26:03 -0500

    use comment for docs preview URL

--- .github/set_docs_preview_url.py
+++ .github/set_docs_preview_url.py
@@ -1,4 +1,3 @@
-import json
 import os
 import re
 
@@ -7,8 +6,7 @@ import httpx
 DEPLOY_OUTPUT = os.environ['DEPLOY_OUTPUT']
 GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
 REPOSITORY = os.environ['REPOSITORY']
-REF = os.environ['REF']
-ENVIRONMENT = 'deploy-preview'
+PULL_REQUEST_NUMBER = os.environ['PULL_REQUEST_NUMBER']
 
 m = re.search(r'https://(\S+)\.workers\.dev', DEPLOY_OUTPUT)
 assert m, f'Could not find worker URL in {DEPLOY_OUTPUT!r}'
@@ -19,6 +17,7 @@ assert m, f'Could not find version ID in {DEPLOY_OUTPUT!r}'
 
 version_id = m.group(1)
 preview_url = f'https://{version_id}-{worker_name}.workers.dev'
+print('Docs preview URL:', preview_url)
 
 gh_headers = {
     'Accept': 'application/vnd.github+json',
@@ -26,30 +25,36 @@ gh_headers = {
     'X-GitHub-Api-Version': '2022-11-28',
 }
 
-deployment_url = f'https://api.github.com/repos/{REPOSITORY}/deployments'
-deployment_data = {
-    'ref': REF,
-    'task': f'docs preview {version_id}',
-    'environment': ENVIRONMENT,
-    'auto_merge': False,
-    'required_contexts': [],
-    'payload': json.dumps({
-        'preview_url': preview_url,
-        'worker_name': worker_name,
-        'version_id': version_id,
-    })
-}
-r = httpx.post(deployment_url, headers=gh_headers, json=deployment_data)
-print(f'POST {deployment_url}: {r.status_code}')  # {r.text}
-r.raise_for_status()
-deployment_id = r.json()['id']
+# now create or update a comment on the PR with the preview URL
 
-status_url = f'https://api.github.com/repos/{REPOSITORY}/deployments/{deployment_id}/statuses'
-status_data = {
-    'environment': ENVIRONMENT,
-    'environment_url': preview_url,
-    'state': 'success',
-}
-r = httpx.post(status_url, headers=gh_headers, json=status_data)
-print(f'POST {status_url}: {r.status_code}')  # {r.text}
+comments_url = f'https://api.github.com/repos/{REPOSITORY}/issues/{PULL_REQUEST_NUMBER}/comments'
+r = httpx.get(comments_url, headers=gh_headers)
+print(f'GET {comments_url} {r.status_code}')
+comment_update_url = None
+
+for comment in r.json():
+    if comment['user']['login'] == 'github-actions[bot]' and comment['body'].startswith('## Docs Preview'):
+        comment_update_url = comment['url']
+        break
+
+body = f"""\
+## Docs Preview
+
+<table>
+<tr>
+<td><strong>Preview URL:</strong></td>
+<td><a href="{preview_url}">{preview_url}</a></td>
+</tr>
+</table>
+"""
+comment_data = {'body': body}
+
+if comment_update_url:
+    print('Updating existing comment...')
+    r = httpx.patch(comment_update_url, headers=gh_headers, json=comment_data)
+else:
+    print('Creating new comment...')
+    r = httpx.post(comments_url, headers=gh_headers, json=comment_data)
+
+print(f'{r.request.method} {comments_url} {r.status_code}')
 r.raise_for_status()
