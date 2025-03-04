d2a6cd401b8398e3b610e1347fc7715e8b120d7e
Benjamin Rapaport <benjamin.rapaport@gmail.com>
2025-03-04 03:18:16 -0500

    Add support for `gemini-2.0-pro-exp-02-05` (#1038)

--- pydantic_ai_slim/pydantic_ai/models/gemini.py
+++ pydantic_ai_slim/pydantic_ai/models/gemini.py
@@ -53,6 +53,7 @@ LatestGeminiModelNames = Literal[
     'gemini-exp-1206',
     'gemini-2.0-flash',
     'gemini-2.0-flash-lite-preview-02-05',
+    'gemini-2.0-pro-exp-02-05',
 ]
 """Latest Gemini models."""
 
