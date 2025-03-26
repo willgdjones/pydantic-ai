0ce5c1b92b12e1aec57d801e4905226480f60160
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-03-26 12:17:17 +0100

    use hyphen in docs (#1245)
    Co-authored-by: Samuel Colvin <s@muelcolvin.com>

--- docs-site/src/index.ts
+++ docs-site/src/index.ts
@@ -3,10 +3,23 @@ export default {
     const r = await env.ASSETS.fetch(request)
     if (r.status == 404) {
       const url = new URL(request.url)
+      const redirectPath = redirect(url.pathname)
+      if (redirectPath) {
+        url.pathname = redirectPath
+        return Response.redirect(url.toString(), 301)
+      }
       url.pathname = '/404.html'
       const r = await env.ASSETS.fetch(url)
-      return new Response(r.body, { status: 404 })
+      return new Response(r.body, { status: 404, headers: {'content-type': 'text/html'} })
     }
     return r
   },
 } satisfies ExportedHandler<Env>
+
+const redirect_lookup: Record<string, string> = {
+  '/common_tools': '/common-tools/',
+}
+
+function redirect(pathname: string): string | null {
+  return redirect_lookup[pathname.replace(/\/+$/, '')] ?? null
+}
