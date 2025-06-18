9c7480d8c402d58ae42f49e62f9f4a0a5a231da3
Tatsuya Shimoda <tacoo@users.noreply.github.com>
2025-06-18 11:06:52 +0900

    Update Google models (#2010)

--- pydantic_ai_slim/pydantic_ai/models/google.py
+++ pydantic_ai_slim/pydantic_ai/models/google.py
@@ -79,8 +79,11 @@ LatestGoogleModelNames = Literal[
     'gemini-2.0-flash-lite-preview-02-05',
     'gemini-2.0-pro-exp-02-05',
     'gemini-2.5-flash-preview-05-20',
+    'gemini-2.5-flash',
+    'gemini-2.5-flash-lite-preview-06-17',
     'gemini-2.5-pro-exp-03-25',
     'gemini-2.5-pro-preview-05-06',
+    'gemini-2.5-pro',
 ]
 """Latest Gemini models."""
 
