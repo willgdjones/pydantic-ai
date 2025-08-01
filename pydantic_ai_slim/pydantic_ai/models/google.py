749dc98c6ab1a4870eb9e721b2402eaa7cc8fe34
Yi-Chen Lin <103916325+ethan01x@users.noreply.github.com>
2025-08-02 00:08:10 +0800

    Rename gemini-2.5-flash-lite-preview-06-17 to gemini-2.5-flash-lite as it's out of preview (#2387)

--- pydantic_ai_slim/pydantic_ai/models/google.py
+++ pydantic_ai_slim/pydantic_ai/models/google.py
@@ -76,7 +76,7 @@ LatestGoogleModelNames = Literal[
     'gemini-2.0-flash',
     'gemini-2.0-flash-lite',
     'gemini-2.5-flash',
-    'gemini-2.5-flash-lite-preview-06-17',
+    'gemini-2.5-flash-lite',
     'gemini-2.5-pro',
 ]
 """Latest Gemini models."""
